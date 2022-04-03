d831e1fdce2a3bd40d0e350f30b02eba7f379c95
Chris <chris.hendricksonacc@gmail.com>
2022-04-03 13:12:49 +1200

    a

--- Delights/DelightsApp/models.py
+++ Delights/DelightsApp/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-
+# models
 class Ingredients(models.Model):
     Ingredient = models.CharField(max_length=200)
     quantity = models.FloatField(default=0)
@@ -18,6 +18,7 @@ class RecipeRequirement(models.Model):
     menu_item = models.ForeignKey(MenuItems, on_delete=models.CASCADE)
     ingredient = models.ForeignKey(Ingredients, on_delete=models.CASCADE)
     quantity = models.FloatField(default = 0)
+
 
 class Purchase(models.Model):
     menu_item = models.ForeignKey(MenuItems,on_delete=models.CASCADE)
