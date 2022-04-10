921bbc79f302032b6f502183206a7a2ebec5db8e
Chris <chris.hendricksonacc@gmail.com>
2022-04-10 20:09:23 +1200

    commit

--- Delights/MessageBoard/views.py
+++ Delights/MessageBoard/views.py
@@ -24,17 +24,32 @@ class MessageView(LoginRequiredMixin, CreateView):
     template_name = 'MessageBoard/messages.html'
     success_url = reverse_lazy("messages")
 
+
+
+
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super().form_valid(form)
 
     def get_context_data(self, **kwargs):
-
+        message_count = len(Message.objects.all())
+        if message_count >= 30:
+          oldest = Message.objects.first()
+          oldest.delete()
         context = super().get_context_data(**kwargs)
         context['messages']= Message.objects.all().reverse()
         context['current_user']=self.request.user
+        context['count']=message_count
         return context
 
 def logout_request(request):
   logout(request)
   return redirect("login")
+
+
+# This is the typical path for a view inheriting from TemplateView.
+
+# as_view returns a function that calls dispatch.
+# dispatch calls get or, if request.method isn't GET, the http_method_not_allowed method.
+# get calls get_context_data and passes that as an argument to render_to_response.
+# render_to_response calls get_template_names and passes that as an argument to TemplateResponse.
